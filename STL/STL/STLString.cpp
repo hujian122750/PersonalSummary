7be490a42f6da50f2d97575ce7c06dfa95bf1c41
hujian122750 <hujian122750@163.com>
2015-10-06 22:21:43 +0800

    stl 字符串的初始化及遍历

--- STL/STL/STLString.cpp
+++ STL/STL/STLString.cpp
@@ -7,11 +7,54 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
-
+int stringInit()
+{
+//    四种初始化字符串的方法
+    string str1 = "STL string1";
+    string str2("STL string2");
+    string str3 = str1;
+    string str4(10,'s');
+    cout<<str1<<endl;
+    cout<<str2<<endl;
+    cout<<str3<<endl;
+    cout<<str4<<endl;
+    
+    return 0;
+}
+//字符串的遍历
+int stringErgodic()
+{
+    string str = "stl string ergodic";
+//    数组方式遍历
+    for(int i = 0; i < str.length(); i++)
+    {
+        cout<<str[i];
+    }
+    cout<<endl;
+//    at遍历
+//    与数组遍历的区别：在数组越界的时候会抛出异常，可通过catch捕捉到，但是
+//    数组越界直接导致程序崩溃，try，catch捕捉不到
+    for(int i = 0; i< str.length(); i++)
+    {
+        cout<<str.at(i);
+    }
+    cout<<endl;
+//    迭代器遍历
+    for(string::iterator it = str.begin(); it != str.end(); it++)
+    {
+        cout<<*it;
+    }
+    cout<<endl;
+    return 0;
+}
 int main(int argc, const char * argv[])
 {
+    int rv = 0;
+//    rv = stringInit();
+    rv = stringErgodic();
 
-    cout<<"*****"<<endl;
+    cout<<"*****STL String*****"<<endl;
     return 0;
 }
